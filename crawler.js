d818f74adc9394c08e38316a99d8f2fbe07e4f44
Vineeth <18vvoruganti.stem@gmail.com>
2017-09-09 09:18:51 -0400

    Initial Bower files and crawler.js production

---
+++ crawler.js
@@ -0,0 +1,10 @@
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://test-project-e424f.firebaseio.com"
+});
+
+var database = firebase.database()
+
+functionb dataScrape() {
+
+}
